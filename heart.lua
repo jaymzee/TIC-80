37d301f401428ba8809413adc5bba35fb91ea9f6
jaymzee <jaymzee@users.noreply.github.com>
2020-09-25 08:53:55 -0400

    heart

---
+++ heart.lua
@@ -0,0 +1,56 @@
+-- title:  heart
+-- author: jaymzee
+-- desc:   plot the 2 times table
+-- script: lua
+
+R=60
+C=40
+F=2
+n=0
+PI=math.pi
+sin=math.sin
+cos=math.cos
+
+cls()
+
+function TIC()
+	local n2,t,t2,x,x2,y,y2
+	if n<C then
+		n2=F*n
+		t=2*PI*n/C
+		t2=2*PI*n2/C
+		x=R*cos(t)
+		x2=R*cos(t2)
+		y=R*sin(t)
+		y2=R*sin(t2)
+		line(120-x,68-y,120-x2,68-y2,5)
+		print(F .. " TIMES TABLE")
+		n=n+1
+	end
+end
+
+-- <TILES>
+-- 001:eccccccccc888888caaaaaaaca888888cacccccccacc0ccccacc0ccccacc0ccc
+-- 002:ccccceee8888cceeaaaa0cee888a0ceeccca0ccc0cca0c0c0cca0c0c0cca0c0c
+-- 003:eccccccccc888888caaaaaaaca888888cacccccccacccccccacc0ccccacc0ccc
+-- 004:ccccceee8888cceeaaaa0cee888a0ceeccca0cccccca0c0c0cca0c0c0cca0c0c
+-- 017:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
+-- 018:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
+-- 019:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
+-- 020:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
+-- </TILES>
+
+-- <WAVES>
+-- 000:00000000ffffffff00000000ffffffff
+-- 001:0123456789abcdeffedcba9876543210
+-- 002:0123456789abcdef0123456789abcdef
+-- </WAVES>
+
+-- <SFX>
+-- 000:000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000304000000000
+-- </SFX>
+
+-- <PALETTE>
+-- 000:1a1c2c5d275db13e53ef7d57ffcd75a7f07038b76425717929366f3b5dc941a6f673eff7f4f4f494b0c2566c86333c57
+-- </PALETTE>
+
